c09260f79839b229d95c406a3a8a5fe87f681380
husseinhesham24 <husseinhesham24@gmail.com>
2024-02-03 22:25:37 +0200

    handing unexpected errors in the SaveSearchJob

--- app/sidekiq/save_search_job.rb
+++ app/sidekiq/save_search_job.rb
@@ -3,8 +3,13 @@ class SaveSearchJob
 
   def perform(search_params, user_id)
     current_user = User.find_by(id: user_id)
-    @search = Search.new(search_params)
-    @search.user = current_user
-    @search.save
+    begin
+      @search = Search.new(search_params)
+      @search.user = current_user
+      @search.save!
+    rescue StandardError => e
+      Rails.logger.error("SaveSearchJob failed: #{e.message}")
+      raise e
+    end
   end
 end
